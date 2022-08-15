5d8b374b1757c1563caf44c4767695165f0fd3a2
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-08-15 19:22:31 +0200

    deleted createUser func.

--- api/users/controller.go
+++ api/users/controller.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"selling_tmp/ent"
 
 	"github.com/gin-gonic/gin"
 )
@@ -32,24 +31,8 @@ func postUser(c *gin.Context) {
 	})
 }
 
-func createUser(c *gin.Context) {
-	var user ent.User
-	if err := c.ShouldBindJSON(&user); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	if user.Name != "manu" || user.Phone != "123" {
-		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
-}
-
 func AddRoutes(parentRoute *gin.Engine) {
 	route := parentRoute.Group("/users")
 	route.GET("/:name/:msg", getUsers)
-	route.POST("/user", createUser)
 	route.POST("/", postUser)
 }
