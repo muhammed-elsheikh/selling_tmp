10d3786edcb637e77b5903a41e5b85f3ff5afe93
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-08-16 13:45:42 +0200

    database connection

--- db/db.go
+++ db/db.go
@@ -2,9 +2,13 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 	"selling_tmp/ent"
 	"selling_tmp/ent/migrate"
+
+	"github.com/joho/godotenv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -13,7 +17,21 @@ var Client *ent.Client
 
 func init() {
 	var err error
-	Client, err = ent.Open("mysql", "akkhor:Ma52569522??@tcp(localhost:3306)/selling_tmp")
+	err = godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	Dbdriver := os.Getenv("DB_DRIVER")
+	DbHost := os.Getenv("DB_HOST")
+	DbUser := os.Getenv("DB_USER")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbName := os.Getenv("DB_NAME")
+	DbPort := os.Getenv("DB_PORT")
+
+	DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DbUser, DbPassword, DbHost, DbPort, DbName)
+	Client, err = ent.Open(Dbdriver, DBURL)
 	if err != nil {
 		log.Fatalf("failed connecting to mysql: %v", err)
 	}
