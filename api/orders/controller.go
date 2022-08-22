3968b8c5ca96fdfab4f3c2d57501f2706e06809a
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-08-22 20:00:59 +0200

    added getOrder, updateOrder and deleteOrder routes.

--- api/orders/controller.go
+++ api/orders/controller.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"selling_tmp/db"
 	"selling_tmp/ent"
+	"selling_tmp/ent/order"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -48,8 +50,62 @@ func createOrder(c *gin.Context) {
 	c.JSON(http.StatusCreated, order)
 }
 
+func getOrder(c *gin.Context) {
+	orderID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	order, err := db.Client.Order.Query().Where(order.ID(orderID)).First(c)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"order_details": order})
+}
+
+func updateOrder(c *gin.Context) {
+	var inputs *ent.Order
+
+	if err := c.BindJSON(&inputs); err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	order, err := db.Client.Order.UpdateOneID(inputs.ID).
+		SetUserID(inputs.UserID).
+		SetProductID(inputs.ProductID).
+		SetQuantity(inputs.Quantity).
+		Save(c)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	c.JSON(http.StatusOK, order)
+}
+
+func deleteOrder(c *gin.Context) {
+	orderID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	order := db.Client.Order.DeleteOneID(orderID)
+
+	c.JSON(http.StatusOK, gin.H{"delete_order": order})
+}
+
 func AddRoutes(parentRoute *gin.Engine) {
 	route := parentRoute.Group("/orders")
 	route.GET("/all", getOrders)
 	route.POST("", createOrder)
+	route.GET("/:id", getOrder)
+	route.PUT("/update", updateOrder)
+	route.DELETE("/:id", deleteOrder)
 }
