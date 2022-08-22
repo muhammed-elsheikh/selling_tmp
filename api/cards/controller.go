72d3ec5b4a32bdd8022708c4c40f06335b5104bd
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-08-22 19:14:23 +0200

    added updateCard, getCard and deleteCard routes.

--- api/cards/controller.go
+++ api/cards/controller.go
@@ -6,6 +6,7 @@ import (
 	"selling_tmp/db"
 	"selling_tmp/ent"
 	"selling_tmp/ent/card"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -51,27 +52,58 @@ func addCard(c *gin.Context) {
 }
 
 func getCard(c *gin.Context) {
-	var inputs *ent.Card
-
-	if err := c.ShouldBindJSON(&inputs); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	card, err := db.Client.Card.
-		Query().
-		Where(card.ID(inputs.ID)).
-		First(c)
+	cardID, err := strconv.Atoi(c.Param("id"))
 
 	if err != nil {
 		fmt.Println(err.Error())
-		c.JSON(http.StatusBadRequest, card)
+		c.AbortWithStatus(http.StatusBadRequest)
 	}
+
+	card, err := db.Client.Card.Query().Where(card.ID(cardID)).First(c)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	c.JSON(http.StatusOK, gin.H{"cardDetails": card})
+}
+
+func updateCard(c *gin.Context) {
+	var inputs *ent.Card
+	if err := c.BindJSON(&inputs); err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	card, err := db.Client.Card.UpdateOneID(inputs.ID).
+		SetUserID(inputs.UserID).
+		SetNumber(inputs.Number).
+		Save(c)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	c.JSON(http.StatusOK, card)
+}
+
+func deleteCard(c *gin.Context) {
+	cardID, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	card := db.Client.Card.DeleteOneID(cardID).Exec(c)
+
+	c.JSON(http.StatusCreated, gin.H{"deleteCard": card})
 }
 
 func AddRoutes(parentRoute *gin.Engine) {
 	route := parentRoute.Group("/cards")
 	route.GET("/all", getCards)
 	route.POST("", addCard)
-	// route.POST("", getCard)
+	route.PUT("/update", updateCard)
+	route.GET("/:id", getCard)
+	route.DELETE("/:id", deleteCard)
 }
