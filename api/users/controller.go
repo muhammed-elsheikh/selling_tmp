1ddb2e55971fa4bc37f91e19f33a441605a51e95
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-08-22 19:13:32 +0200

    added getUse, updateUser and deleteUser routes.

--- api/users/controller.go
+++ api/users/controller.go
@@ -6,6 +6,7 @@ import (
 	"selling_tmp/db"
 	"selling_tmp/ent"
 	"selling_tmp/ent/user"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -76,9 +77,65 @@ func login(c *gin.Context) {
 	c.JSON(200, user)
 }
 
+func updateUser(c *gin.Context) {
+	var inputs *ent.User
+
+	if err := c.BindJSON(&inputs); err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	user, err := db.Client.User.
+		UpdateOneID(inputs.ID).
+		SetName(inputs.Name).
+		Save(c)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	c.JSON(http.StatusOK, user)
+}
+
+func getUserID(c *gin.Context) {
+	userID, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	user, err := db.Client.User.Query().Where(user.ID(userID)).First(c)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"user_details": user})
+}
+
+func deleteUserID(c *gin.Context) {
+
+	userID, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	deleteUser := db.Client.User.DeleteOneID(userID).Exec(c)
+
+	c.JSON(http.StatusCreated, gin.H{"deletedUser": deleteUser})
+}
+
 func AddRoutes(parentRoute *gin.Engine) {
 	route := parentRoute.Group("/users")
 	route.GET("/all", getUsers)
 	route.POST("/register", register)
 	route.POST("/login", login)
+	route.PUT("/update", updateUser)
+	route.GET("/:id", getUserID)
+	route.DELETE("/:id", deleteUserID)
 }
