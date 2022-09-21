806293025c73dc3c006d52e5f865ad72c49f0abb
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-09-21 13:03:29 +0200

    import data from JSON file to local DB

--- seed/seeds.go
+++ seed/seeds.go
@@ -7,25 +7,34 @@ import (
 	"selling_tmp/db"
 	"selling_tmp/ent"
 
+	"github.com/gin-gonic/gin"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/mitchellh/mapstructure"
 )
 
 func Do(ctx context.Context) error {
-	var users []*ent.User
-
-	jsonFile, err := os.Open("/home/mohammed/Desktop/selling_tmp/public/MOCK_DATA.json")
+	var usersJSON []gin.H
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	jsonFile, err := os.ReadFile("/home/mohammed/Desktop/selling_tmp/public/MOCK_DATA.json")
 
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+
 	fmt.Println("Successfully Opened MOCK_DATA.json")
 
-	err = mapstructure.Decode(jsonFile, &users)
+	err = json.Unmarshal(jsonFile, &usersJSON)
+
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	for _, user := range users {
+	for _, u := range usersJSON {
+		var user ent.User
+		err := mapstructure.Decode(u, &user)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
 		db.Client.User.Create().
 			SetName(user.Name).
 			SetUsername(user.Username).
